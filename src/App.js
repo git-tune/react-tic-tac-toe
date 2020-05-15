29fc765d601c2c0079cebbed543f9a748620c968
久恒大樹 <58614957+tunetune@users.noreply.github.com>
2020-05-16 05:16:38 +0900

    develop

--- src/App.js
+++ src/App.js
@@ -1,112 +1,162 @@
 import React from 'react';
-import './App.css';
+import styled from 'styled-components';
+import GlobalStyle from './GlobalStyle';
+import Board from './components/Board';
+import Turn from './components/Turn';
 
-function Square(props) { 
-  return (
-      <button 
-        className="square"
-        onClick={props.onClick}
-      >
-        {props.value}
-      </button>
-    );
-}
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`;
 
-class Board extends React.Component {
+const Header = styled.div`
+  padding: 16px;
+`;
+
+const Title = styled.h1`
+  text-align: center;
+`;
+
+const Content = styled.div``;
+
+const Footer = styled.div`
+  padding: 16px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const Button = styled.a`
+  display: inline-block;
+  text-align: center;
+  font-weight: bold;
+  border: 3px solid black;
+  border-radius: 6px;
+  padding: 4px 16px;
+  &:hover {
+    background: black;
+    color: white;
+    cursor: pointer;
+  }
+`;
+
+const StatusText = styled.div`
+  text-align: center;
+  padding: 8px;
+`;
+
+const CHAR = {
+  circle: '○',
+  cross: '×',
+};
+
+const STATUS_TEXT = {
+  progressing: 'progressing',
+  win: 'win',
+  draw: 'draw',
+};
+
+const initialState = {
+  winner: null,
+  turn: CHAR.circle,
+  progress: true,
+  statusText: STATUS_TEXT.progressing,
+  handCount: 0,
+  isCircleTurn: true,
+  board: new Array(9),
+};
+
+export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true
-                 };
+    this.state = { ...initialState };
   }
-  
-  handleClick(i) {
-    const squares = this.state.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
+
+  onRestart = () => {
+    this.setState({ ...initialState });
+  };
+
+  onClick = (index) => {
+    const { board, handCount, progress, turn } = this.state;
+    if (board[index] || !progress) {
       return;
     }
-    squares[i] = this.state.xIsNext ? '×' : '○';
+
+    const newBoard = [...board];
+    newBoard[index] = turn;
     this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext
+      board: newBoard,
+      handCount,
+      turn: turn === CHAR.circle ? CHAR.cross : CHAR.circle,
     });
-  }
-  
-  renderSquare(i) {
-    return  (
-           <Square 
-              value={this.state.squares[i]}
-              onClick={() => this.handleClick(i)}
-           />
-      );
-  }
 
-  render() {
-    const winner = calculateWinner(this.state.squares);
-    let status;
-    if (winner) {
-      status = 'Winner: ' + winner;
-    } else {
-      status = "Next player: " + (this.state.xIsNext ? '×' : '○');
+    if (checkWinner(newBoard, turn, index)) {
+      this.setState({
+        progress: false,
+        winner: turn,
+        statusText: turn + ' ' + STATUS_TEXT.win,
+      });
+      return;
     }
 
+    if (checkDraw(newBoard)) {
+      this.setState({
+        progress: false,
+        statusText: STATUS_TEXT.draw,
+      });
+      return;
+    }
+  };
+
+  render() {
+    const { turn, board, statusText } = this.state;
+
     return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
+      <Container>
+        <Content>
+          <GlobalStyle />
+          <Header>
+            <Title>Tic Tac Toe</Title>
+            <Turn turns={Object.values(CHAR)} turn={turn} />
+          </Header>
+          <Board board={board} onClick={this.onClick} />
+          <Footer>
+            <StatusText>{statusText}</StatusText>
+            <Button onClick={this.onRestart}>Restart</Button>
+          </Footer>
+        </Content>
+      </Container>
     );
   }
 }
 
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
+const checkWinner = (board, value) => {
+  //   const lines = [
+  //     [0, 1, 2],
+  //     [3, 4, 5],
+  //     [6, 7, 8],
+  //     [0, 3, 6],
+  //     [1, 4, 7],
+  //     [2, 5, 8],
+  //     [0, 4, 8],
+  //     [2, 4, 8],
+  //   ];
+  //   return lines.every((item) => board[item] === value);
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Tic Tac Toe</h1>
-        <div className="game">
-          <div className="game-board">
-            <Board />
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
+  return (
+    [0, 1, 2].every((item) => board[item] === value) ||
+    [3, 4, 5].every((item) => board[item] === value) ||
+    [6, 7, 8].every((item) => board[item] === value) ||
+    [0, 3, 6].every((item) => board[item] === value) ||
+    [1, 4, 7].every((item) => board[item] === value) ||
+    [2, 5, 8].every((item) => board[item] === value) ||
+    [0, 4, 8].every((item) => board[item] === value) ||
+    [2, 4, 6].every((item) => board[item] === value)
+  );
+};
 
-export default App;
+const checkDraw = (board) => {
+  return board.every((item) => item);
+};
